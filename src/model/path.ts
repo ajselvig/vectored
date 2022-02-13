506631db9b2fba3f97ef1579f722ac0124fcacbb
Andy Selvig <ajselvig@gmail.com>
2022-02-12 19:18:00 -0600

    start of path data model

--- src/model/path.ts
+++ src/model/path.ts
@@ -1,8 +1,32 @@
 
 import { ProjectModel } from './model'
 import Project from "./project"
+import * as geom from '../util/geom'
 
+
+export type VertexType = 'point' | 'symmetric' | 'asymmetric' | 'disjoint'
+
+/**
+ * Represents a single vertex with control points going in and out.
+ */
+export class Vertex {
+    type: VertexType = 'point'
+    point = geom.point()
+    controlOut = geom.vector()
+    controlIn = geom.vector()
+}
+
+
+export type OpenOrClosed = 'open' | 'closed'
+
+/**
+ * A series of vertices forming an open or closed path of straight and/or curved edges.
+ */
 export default class Path extends ProjectModel<never> {
+
+    vertices = Array<Vertex>()
+
+    openOrClosed: OpenOrClosed = 'open'
 
     constructor(readonly project: Project, id?: string) {
         super('path', project, id)
--- src/util/geom.ts
+++ src/util/geom.ts
@@ -1,19 +1,37 @@
 
 import Flatten from '@flatten-js/core'
-// const vector = Flatten.vector
-// const point = Flatten.point
 type RawPoint = [number, number]
+
+
+/// Point Helpers
+
+export type Point = Flatten.Point
+
+export const point = Flatten.point
+
+
+/// Vector Helpers
+
+export type Vector = Flatten.Vector
+
+export const vector = Flatten.vector
+
+
+// Box Helpers
 
 export const box = (xmin?: number, ymin?: number, xmax?: number, ymax?: number): Flatten.Box => {
     return new Flatten.Box(xmin, ymin, xmax, ymax)
 }
 
-export const identityMatrix = (): Flatten.Matrix => {
-    return new Flatten.Matrix(1, 0, 0, 1, 0, 0)
-}
-
 export const boxSize = (box: Flatten.Box): Flatten.Vector => {
     return new Flatten.Vector(box.xmax-box.xmin, box.ymax-box.ymin)
+}
+
+
+// Matric Helpers
+
+export const identityMatrix = (): Flatten.Matrix => {
+    return new Flatten.Matrix(1, 0, 0, 1, 0, 0)
 }
 
 export const transformBox = (box: Flatten.Box, transform: Flatten.Matrix): Flatten.Box => {
